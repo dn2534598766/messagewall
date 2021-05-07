c7cd04dec9ef774465f2bb84a8f5da2a14495009
dn2534598766 <2534598766@qq.com>
2021-05-07 10:37:30 +0800

    commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express=require('express')
 const multer=require('multer')
 const watermark=require('./watermark.js')
+const bodyParser=require('body-parser')
+const Mock=require('mockjs')
+
 
 const updir='./uploads'
 let app=express()
 app.use(express.static(updir))
 app.use(express.static('.'))
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+app.use(bodyParser.json())
 
 let storage=multer.diskStorage({
     destination:function(req,file,cb){
@@ -31,4 +38,49 @@ app.post('/watermark',upload.single('file'),function(req,res,next){
     res.send('bg.png')
 })
 
-app.listen(80)
+let data={
+    sno:'07190999',
+    name:'',
+    gender:1,
+    skills:[
+        {
+            name:'HTML',
+            count:0
+        },
+        {
+            name:'CSS',
+            count:0
+        },
+        {
+            name:'javascript',
+            count:0
+        },
+        {
+            name:'Vuejs',
+            count:0
+        },
+        {
+            name:'miniprogram',
+            count:0
+        }
+    ],
+    satisfaction:0,
+    opinion:'',
+    count:0
+}
+
+app.get('/sampledata',(req,res)=>{
+    res.json(data)
+})
+app.post('/sampledata',(req,res)=>{
+    console.log(req.body)
+    data.skills.forEach(v=>{
+        req.body.skills.indexOf(v.name)>-1?v.count++:""
+    })
+    data.satisfaction=(data.satisfaction*data.count+req.body.satisfaction)/(data.count+1)
+    data.count++
+    data.name=Mock.mock('@cname')
+    data.opinion=Mock.mock('@cparagraph')
+    res.json(data)
+})
+app.listen(8080)
